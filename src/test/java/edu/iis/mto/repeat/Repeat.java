c0c7b8cb7a41234b25c5ec656237653f4e5b9adf
Karol Kucharski <187746@edu.p.lodz.pl>
2016-05-19 16:58:53 +0200

    Dodanie implementacji reguly wieloktornego wykonania testu.

---
+++ src/test/java/edu/iis/mto/repeat/Repeat.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.repeat;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD })
+public @interface Repeat {
+	public abstract int times();
+}
