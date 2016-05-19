5d254bb21e9055532fb7c8cd8969d6311f2aae3c
Karol Kucharski <187746@edu.p.lodz.pl>
2016-05-19 17:01:23 +0200

    1000krotne wykonywanie testu w RadarTest.

--- src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,10 +3,19 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import edu.iis.mto.repeat.Repeat;
+import edu.iis.mto.repeat.RepeatRule;
+
 public class RadarTest {
+	
+	@Rule
+	public RepeatRule repeatRule = new RepeatRule();
+	
 	@Test
+	@Repeat(times = 1000)
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
 		Radar radar = new Radar(batteryMock);
